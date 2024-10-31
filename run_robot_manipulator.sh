c4b89265fa12714be173d336cc8138b74a4f2ed2
Benjamin Teh <benjamintehjh01@gmail.com>
2024-10-31 09:53:56 +0800

    run container script

---
+++ run_robot_manipulator.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+container_name="rb_manipulator"
+
+if [ "$(docker ps -aq -f status=running -f name=${container_name})" ]
+then
+	echo "Container is Running. Starting new session."
+	docker exec -it ${container_name} bash 
+else
+    echo "Starting new container."
+    docker run -it --rm \
+    --name ${container_name} \
+    -h ${container_name} \
+    --network=host \
+	--volume="$(pwd)/ros2_ws":"/root/ros2_ws" \
+    bentjh01:f1tenth-foxy
+fi
